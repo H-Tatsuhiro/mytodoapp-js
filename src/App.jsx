237bbe89de75e43eb96364ce277140a9a74e1d16
H-Tatsuhiro <admin@h-tatsu.com>
2023-05-15 13:41:17 +0900

    React-Hook-Formの適用（TODO機能の正常化） Close #2

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,6 @@ import TodoAddForm from "./components/TodoAddForm";
 function App() {
     return (
         <div>
-            <h1>hello, react</h1>
             <TodoViewForm />
             <TodoAddForm />
         </div>
--- src/components/TodoAddForm.jsx
+++ src/components/TodoAddForm.jsx
@@ -1,12 +1,37 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import { useForm } from "react-hook-form";
+
 function TodoAddForm() {
+    const { register, handleSubmit, formState: { errors }} = useForm({
+        mode: "all"
+    });
+
+    const onSubmit = (data) => {
+        const newDiv = document.createElement("div");
+        newDiv.className = "todos";
+        newDiv.id = "div-" + data.todoName;
+        const newTodoInput = document.createElement("input");
+        newTodoInput.type = "checkbox";
+        newTodoInput.id = data.todoName;
+        newTodoInput.className = "todoinput";
+        newTodoInput.name = data.todoName;
+        newDiv.appendChild(newTodoInput);
+        newTodoInput.checked = true;
+        const newTodoLabel = document.createElement("label");
+        newTodoLabel.htmlFor = data.todoName;
+        newTodoLabel.textContent = data.todoName;
+        newDiv.appendChild(newTodoLabel);
+        var pardiv = document.getElementById("last");
+        pardiv.before(newDiv);
+    };
+
     return (
         <div>
             <p>todo追加</p>
-            <form name="addtodoForm" id="addtodoForm">
+            <form name="addtodoForm" id="addtodoForm" onSubmit={handleSubmit(onSubmit)}>
                 <label htmlFor="addtodo">todo名</label>
-                <input type="text" id="addtodo" name="addtodo" />
+                <input type="text" id="addtodo" {...register('todoName', { required: true })}/>
+                {errors.todoName && <div>入力必須項目</div>}
                 <button type="submit" id="subbtn">追加</button>
             </form>
         </div>
